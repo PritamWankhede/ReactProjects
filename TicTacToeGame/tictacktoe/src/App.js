12439bcb7b43bd4941c5f090222a9493ea26f96e
PritamWankhede <pritamwankhede671@gmail.com>
2025-03-08 02:05:59 +0530

    Added Tic-Tak-Toe project

---
+++ TicTacToeGame/tictacktoe/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TicTakToe from './Component/TikTacToe/TicTakToe';
+
+function App() {
+  return (
+    <div>
+     <TicTakToe/>
+    </div>
+  );
+}
+
+export default App;
